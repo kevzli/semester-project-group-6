6c01b068334cf8146a23b23bf280ffbcc6be127b
ShawnZhu03 <shawn.z@wustl.edu>
2024-02-25 20:12:26 -0600

    finished add trips page

---
+++ components/Trending.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import styles from "./Trending.module.css";
+import { ref, getDatabase, onValue, orderByChild, equalTo } from "firebase/database";
+import { db } from "../firebase/firebase";
+import { useAuth } from "../firebase/auth";
+import TripStockPhoto from "../public/trip-stock-photo.jpg"
+
+export default function Trending() {
+  const { authUser } =  useAuth();
+  const [userTrips, setUserTrips] = useState([]);
+
+  useEffect(() => {
+    // Check if authUser is available before fetching trips
+    if (authUser?.uid) {
+      const tripDatabaseRef = ref(db, "trips/");
+      const fetchUserTrips = () => {
+        onValue(tripDatabaseRef, (snapshot) => {
+          const trips = [];
+          snapshot.forEach((childSnapshot) => {
+            const trip = childSnapshot.val();
+            if (trip.trip_owner === authUser.uid) {
+              trips.push({
+                tripId: childSnapshot.key,
+                ...trip,
+              });
+            }
+          });
+          setUserTrips(trips);
+        });
+      };
+      fetchUserTrips();
+    }
+  }, [authUser]);
+
+  console.log(userTrips);
+
+  return (
+    <section className={styles.trending}>
+      <div className={styles.heading}>
+        <h2>Trips 2024</h2>
+        <p>Description</p>
+      </div>
+      <div className={styles.wrapper}>
+        {userTrips.map((trip) => (
+          <div key={trip.tripId} className={styles.card}>
+            <div className={styles.cardHeader}>
+              <img src={TripStockPhoto} alt={trip.trip_name} className={styles.cardImg} />
+              <h3 className={styles.cardTitle}>{trip.name}</h3>
+              <p className={styles.cardLocation}>{trip.location}</p>
+            </div>
+            <div className={styles.cardBody}>
+              <div className={styles.cardInfo}>
+                <span>{trip.trip_dest}</span>
+                <span>Start Date: {trip.start_date} Places</span>
+                <span>End: {trip.end_date}</span>
+              </div>
+            </div>
+            <button className={styles.cardButton}>Enter</button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
