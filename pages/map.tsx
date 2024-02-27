47d825720f91594d25513e2c5f2384fe721fe5b9
yurock23 <112423753+Paul-Heo@users.noreply.github.com>
2024-02-27 17:09:52 -0600

    Update map.tsx

--- pages/map.tsx
+++ pages/map.tsx
@@ -91,7 +91,7 @@ export default function Map() {
                 icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
               />
 
-              {/* <MarkerClusterer>
+              <MarkerClusterer>
                 {(clusterer) =>
                   houses.map((house) => (
                     <Marker
@@ -104,8 +104,7 @@ export default function Map() {
                     />
                   ))
                 }
-              </MarkerClusterer> */}
-
+              </MarkerClusterer>
               <Circle center={office} radius={15000} options={closeOptions} />
               <Circle center={office} radius={30000} options={middleOptions} />
               <Circle center={office} radius={45000} options={farOptions} />
